abd9add05cb2367fa36d9bfbc087a5277f28dd8a
Lorongxz <lorongxz.wfh@gmail.com>
2026-03-04 01:46:11 +0800

    Allow Vercel origin in CORS

--- fildas-backend/config/cors.php
+++ fildas-backend/config/cors.php
@@ -19,7 +19,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'https://fildas-v2-mzlfudkuh-lorongxzs-projects.vercel.app',
+    ],
 
     'allowed_origins_patterns' => [],
 
